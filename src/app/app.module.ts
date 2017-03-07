e881530d3dfe68c011a4c14a13a49826c42ffe6e
João Henrique <joaohfeng@gmail.com>
2017-03-07 19:16:32 -0300

    Mudanças
    - We created a service class that can be shared by many components.
    - We used the ngOnInit Lifecycle Hook to get our heroes when our
    - AppComponent activates.
    - We defined our HeroService as a provider for our AppComponent.
    - We created mock hero data and imported them into our service.
    - We designed our service to return a Promise and our component to get our data from the Promise.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,16 @@ import { HeroDetailComponent  } from './hero-detail.component';
 
 @NgModule({
   imports:[
-	BrowserModule,
-	FormsModule
+    	BrowserModule,
+    	FormsModule
   ],
 
  declarations:[
-	AppComponent,
-	HeroDetailComponent
+    	AppComponent,
+    	HeroDetailComponent
   ],
 
   bootstrap:[
-	AppComponent
-  ]
-
+	AppComponent ]
 })
 export class AppModule { }
