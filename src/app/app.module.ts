a805ac21bfbea0fb157ad9494e872e2d414b97b3
João Henrique <joaohfeng@gmail.com>
2017-03-20 16:31:18 -0300

    Adicionando Routes
        Foi importado o "RouterModule @angular/router". A estrutura
        RouterModule.forRoot([{route},{rote},...]) cada route é composta de
        'path:'e um 'component'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,50 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }         from './app.component';
 import { HeroDetailComponent  } from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+
+import { RouterModule }         from '@angular/router';
+import { DashboardComponent }   from './dashboard.component';
 
 @NgModule({
   imports:[
-    	BrowserModule,
-    	FormsModule
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot([
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: '',
+                redirectTo: '/dashboard',
+                pathMatch: 'full'
+            },
+            {
+                path: 'detail/:id',
+                component: HeroDetailComponent
+            }
+      ])
   ],
 
  declarations:[
-    	AppComponent,
-    	HeroDetailComponent
+        AppComponent,
+        DashboardComponent,
+        HeroDetailComponent,
+        HeroesComponent
+  ],
+  providers:[
+      HeroService
   ],
 
   bootstrap:[
-	AppComponent ]
+      AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+
+}
