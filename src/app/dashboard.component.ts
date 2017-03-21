18e381ddd9d0018a164147ab4cadca0dd643a716
João Henrique <joaohfeng@gmail.com>
2017-03-21 12:49:33 -0300

    Selecionar na Dashboard
    A dashboard foi modificada na tag <div> que continha o ngFor para tag
    <a> e adicionado o comando [routerLink]="['/detail', hero.id]" para
    que ao clicar no hero ele navegue para o component herodetail.

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -12,11 +12,11 @@ import { HeroService  } from './hero.service';
       templateUrl: '.dashboard.component.html',
       template:`<h3>Top Heroes</h3>
                 <div class="grid grid-pad">
-                    <div *ngFor="let hero of heroes" class="col-1-4">
-                        <div class="module hero">
+                    <a *ngFor="let hero of heroes" [routerLink]="['/detail',hero.id]"class="col-1-4">
+                        <a class="module hero">
                             <h4>{{hero.name}}</h4>
-                        </div>
-                    </div>
+                        </a>
+                    </a>
                 </div>
       `
 
