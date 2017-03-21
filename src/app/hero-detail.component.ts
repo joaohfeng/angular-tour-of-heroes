40fd6f2542f0c4767a64052c3e21e10ddd33b33d
João Henrique <joaohfeng@gmail.com>
2017-03-21 13:12:26 -0300

    Modificaçoes no hero-detail.component
    -Importados os components:ActivatedRoute, Params(Para podermos criar uma route que
    possibilitara que o hero detail seja chamado por outro component), Oninit, Location, HeroServiçe;
    -Constructor -ActivatedRoute, HeroService,Location;
    -ngOnInit() void- para priorisar a busca das infomaçoes do Hero;
    -Implementada a funçao getHero derivada da getHeroes onde faz uma busca
    por id;
    -Implmenteda funçao goBack- atravez do serviço ocation é possivel voltar
    um estado anterior do historico do browser;
    -Botao com a funçao goback na template;
    -Adicionado "templateUrl: './hero-detail.component.html'" para
    recarregar a pagina.

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,27 +1,55 @@
-import { Component, Input  } from '@angular/core';
-import { Hero } from './hero';
+import { Component, Input, OnInit  } from '@angular/core';
+import { ActivatedRoute, Params }    from '@angular/router';
+import { Location }                  from '@angular/common';
+
+import { Hero }                      from './hero';
+
+import { HeroService }               from './hero.service';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
-      selector: 'my-hero-detail',
-      template: `
+    moduleId: module.id,
+    selector: 'my-hero-detail',
+    templateUrl: './hero-detail.component.html',
+    template: `
         <div *ngIf="hero">
             <h2>{{hero.name}} details!</h2>
-            <div><label>id: </label>{{hero.id}}</div>
             <div>
-                 <label>name: </label>
-                 <input [(ngModel)]="hero.name" placeholder="name"/>
+
+                <label>id: </label>{{hero.id}}
+
             </div>
+            <div>
+
+                <label>name: </label>
+                <input [(ngModel)]="hero.name" placeholder="name"/>
+
+            </div>
+
+            <button (click)="goBack()">Back</button>
+
         </div>
-      `,
+      `
 
 })
-export class HeroDetailComponent {
-  @Input()
-  hero: Hero;
+export class HeroDetailComponent implements OnInit {
+    @Input() hero: Hero;
+    constructor(
+        private heroService: HeroService,
+        private route: ActivatedRoute,
+        private location: Location
+    ){}
+
+    ngOnInit(): void {
+        this.route.params
+        .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+        .subscribe(hero => this.hero = hero);
+    }
+
+    goBack(): void{
+        this.location.back();
+    }
+
 
 }
-
-
-
-
-
