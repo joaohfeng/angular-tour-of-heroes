f44ca1a93c6568d639bcb404dea1fb8f4d1e18ae
João Henrique <joaohfeng@gmail.com>
2017-03-23 13:13:18 -0300

    Inclusao dos arquivos js e map
        Arquivos.js e Arquivos.map
    
        OBS:LEMBRAR DE ENVIA-LOS JUNTO COM O ARQUIVO.TS

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -13,6 +13,10 @@ var platform_browser_1 = require('@angular/platform-browser');
 var forms_1 = require('@angular/forms');
 var app_component_1 = require('./app.component');
 var hero_detail_component_1 = require('./hero-detail.component');
+var heroes_component_1 = require('./heroes.component');
+var hero_service_1 = require('./hero.service');
+var router_1 = require('@angular/router');
+var dashboard_component_1 = require('./dashboard.component');
 var AppModule = (function () {
     function AppModule() {
     }
@@ -20,14 +24,35 @@ var AppModule = (function () {
         core_1.NgModule({
             imports: [
                 platform_browser_1.BrowserModule,
-                forms_1.FormsModule
+                forms_1.FormsModule,
+                router_1.RouterModule.forRoot([
+                    {
+                        path: 'dashboard',
+                        component: dashboard_component_1.DashboardComponent
+                    },
+                    {
+                        path: '',
+                        redirectTo: '/dashboard',
+                        pathMatch: 'full'
+                    },
+                    {
+                        path: 'detail/:id',
+                        component: hero_detail_component_1.HeroDetailComponent
+                    }
+                ])
             ],
             declarations: [
                 app_component_1.AppComponent,
-                hero_detail_component_1.HeroDetailComponent
+                dashboard_component_1.DashboardComponent,
+                hero_detail_component_1.HeroDetailComponent,
+                heroes_component_1.HeroesComponent
+            ],
+            providers: [
+                hero_service_1.HeroService
             ],
             bootstrap: [
-                app_component_1.AppComponent]
+                app_component_1.AppComponent
+            ]
         }), 
         __metadata('design:paramtypes', [])
     ], AppModule);
