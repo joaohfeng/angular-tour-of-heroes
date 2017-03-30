eab1e1890b46819d8942b1b39d8b5821f117042e
João Henrique <joaohfeng@gmail.com>
2017-03-30 13:41:57 -0300

    Implementando as  Habilidades dos Herois
    -criado os campos com as habilidaes dos herois na classe hero;
    -Atualizando a lista de herois com seus status;

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,5 +1,12 @@
 export class Hero {
-      id: number;
-      name: string;
+    id: number;
+    name: string;
+    Str: number;
+    Dex: number;
+    Con: number;
+    Int: number;
+    Wis: number;
+    Cha: number;
+
 
 }
--- src/app/mock-heroes.js
+++ src/app/mock-heroes.js
@@ -1,14 +1,14 @@
 "use strict";
 exports.HEROES = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+    { id: 11, name: 'Mr. Nice', Str: 9, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10 },
+    { id: 12, name: 'Narco', Str: 10, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10 },
+    { id: 13, name: 'Bombasto', Str: 10, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10 },
+    { id: 14, name: 'Celeritas', Str: 10, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10 },
+    { id: 15, name: 'Magneta', Str: 10, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10 },
+    { id: 16, name: 'RubberMan', Str: 10, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10 },
+    { id: 17, name: 'Dynama', Str: 10, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10 },
+    { id: 18, name: 'Dr IQ', Str: 10, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10 },
+    { id: 19, name: 'Magma', Str: 10, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10 },
+    { id: 20, name: 'Tornado', Str: 10, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10 }
 ];
 //# sourceMappingURL=mock-heroes.js.map
